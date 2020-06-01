0c6ad7a4a847a2f3553c4b614c5831fe22f40ebd
Jacek Kolasa <jacek.kolasa@gmail.com>
2020-06-01 11:32:51 +0200

    configure craco, styled-components, remove CRA boilerplate

---
+++ src/components/custom-dashboard/custom-dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { FirstComponent } from "./styled"
+
+interface Props {
+
+}
+export const CustomDashboard = ({
+
+}: Props) => {
+  return (
+    <FirstComponent>Custom Dashboard</FirstComponent>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './App';
+import { CustomDashboard } from './components/custom-dashboard';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CustomDashboard />
   </React.StrictMode>,
   document.getElementById('root')
 );
